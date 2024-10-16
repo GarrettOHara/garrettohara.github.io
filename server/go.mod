130db7dd76aa6ad6ba738c735426c39ce7c1e8fd
Garrett O'Hara <garrett.ohara@nbcuni.com>
2024-10-16 12:37:22 -0700

    adding go server

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/garrettohara/garrettohara
+
+go 1.22.4
---
+++ server/main.go
@@ -0,0 +1,10 @@
+package main
+
+include (
+    "fmt"
+    "http/net"
+    )
+
+func main(){
+    fmt.print("hello world\n")
+}
