d1e55ce38545e1d4317028e48a9184f04aa8160e
Garrett O'Hara <garrett.ohara@nbcuni.com>
2025-05-27 16:39:50 -0700

    adding path-finding application in javascript

--- server/main.go
+++ server/main.go
@@ -1,10 +1,35 @@
 package main
 
-include (
-    "fmt"
-    "http/net"
-    )
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main(){
-    fmt.print("hello world\n")
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Client IP: %s, Method: %s, URL: %s, User-Agent: %s",
+		r.RemoteAddr, r.Method, r.URL.Path, r.UserAgent())
+
+	w.Header().Set("Content-Type", "text/html")
+	html := "<!DOCTYPE html><html><head><title>Garrett O'Hara</title></head><body><p>hello world</p></body></html>"
+	if _, err := w.Write([]byte(html)); err != nil {
+		log.Printf("Error writing response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	// Return a 204 No Content response for favicon requests
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func main() {
+	portno := flag.Int("p", 8080, "Specify the port number to use")
+	flag.Parse()
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+
+	fmt.Printf("Starting server on port %d...\n", *portno)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portno), nil))
 }
